e7745eb6eef0173b1e8792a5981a05deb70a969d
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2022-11-03 14:19:02 +0300

    added postresql DB changed parcer from package to module

--- app/sql_app/database.py
+++ app/sql_app/database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.sqlite"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+from ..loader import DATABASE_URL, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_PROD_DB_NAME
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.sqlite"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DATABASE_URL}/{POSTGRES_PROD_DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base(bind=engine)
