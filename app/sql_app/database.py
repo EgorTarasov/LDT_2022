bad7f97b5f7c0dc379c754d1c16b6e80565ea098
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2022-11-03 16:38:03 +0300

    fixed docker-compose.yml

--- app/sql_app/database.py
+++ app/sql_app/database.py
@@ -4,9 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 from ..loader import DATABASE_URL
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.sqlite"
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DATABASE_URL}/{POSTGRES_PROD_DB_NAME}"
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=True, autoflush=False, bind=engine)
 
